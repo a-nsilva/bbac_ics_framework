d4dc2b6b50a051bd8eac6ce9bf94d55249ebda50
Alexandre do Nascimento Silva <121621080+a-nsilva@users.noreply.github.com>
2026-01-17 20:56:16 -0300

    Update __init__.py

--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -2,17 +2,18 @@
 BBAC Framework - Experiments Module
 """
 
-from .run import ExperimentRunner
-from .scenarios import ScenarioManager, BASELINE_SCENARIOS
-from .metrics import MetricsCollector
 from .ablation import AblationStudy
 from .baseline_comparison import BaselineComparison
+from .metrics import MetricsCollector
+from .run import ExperimentRunner
+from .scenarios import ScenarioManager
+from .visualization import ResultsPlotter
 
 __all__ = [
-    'ExperimentRunner',
-    'ScenarioManager',
-    'BASELINE_SCENARIOS',
-    'MetricsCollector',
     'AblationStudy',
     'BaselineComparison'
+    'ExperimentRunner',
+    'MetricsCollector',
+    'ResultsPlotter',
+    'ScenarioManager',
 ]
